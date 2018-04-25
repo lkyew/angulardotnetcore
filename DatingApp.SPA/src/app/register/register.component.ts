5bedea0289d97eb2a735105c80cd5d92dd10ca4d
lkyew <35022737+lkyew@users.noreply.github.com>
2018-04-25 18:44:59 +0800

    Add files via upload

---
+++ DatingApp.SPA/src/app/register/register.component.ts
@@ -0,0 +1,28 @@
+import { Component, EventEmitter, OnInit, Input, Output } from '@angular/core';
+
+
+@Component({
+  selector: 'app-register',
+  templateUrl: './register.component.html',
+  styleUrls: ['./register.component.css']
+})
+export class RegisterComponent implements OnInit {
+model: any = {};
+@Input() valuesFromHome: any;
+@Output() cancelRegister = new EventEmitter();
+
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+  register() {
+    console.log(this.model);
+  }
+
+  cancel() {
+    this.cancelRegister.emit(false);
+    console.log('cancelled');
+  }
+}
